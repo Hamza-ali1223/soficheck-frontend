d405a172ad38d27d0b9232299f2edd820d9f7281
Hamza Ali <saira21perveen@gmail.com>
2026-03-05 17:15:11 +0500

    feat : add vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HeroUIProvider } from "@heroui/system";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import App from "./App.tsx";
 import "@/styles/globals.css";
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HeroUIProvider>
       <App />
+      <SpeedInsights />
     </HeroUIProvider>
   </React.StrictMode>,
 );
